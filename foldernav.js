3fea10a2ca938540269e02641a44f64f5560a62a
tinitiate.com <tinitiate@users.noreply.github.com>
2018-03-13 12:54:42 -0400

    1

---
+++ foldernav.js
@@ -0,0 +1,22 @@
+var foldenavjson = {
+"home": {"display":"tinitiate","url":"http://www.tinitiate.com/"},
+"folder": {"display":"java","url":"/java-design-patterns"},
+"nav": [
+    {
+    "display": "introduction"
+    ,"url": "index.html"
+    },
+    {
+    "display": "Factory Method Pattern"
+    ,"url": "factory-method-pattern.html"
+    },
+    {
+    "display": "Singleton Pattern"
+    ,"url": "singleton-pattern.html"
+    },
+    {
+    "display": "Builder Pattern"
+    ,"url": "builder-pattern.html"
+    }
+  ]
+};
